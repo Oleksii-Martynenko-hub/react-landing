de82053237530d8af2317e651049fde5ba97df26
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-05-06 15:24:57 +0300

    Add texts to about section

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -1,18 +1,23 @@
 import styled from 'styled-components';
+
 import Vectors from './vectors';
+import MainContent from './main-content';
 
 /* eslint-disable-next-line */
 export interface AboutProps {}
 
 const StyledAbout = styled.div`
-  color: pink;
+  display: flex;
+  justify-content: space-between;
+  padding: 283px 173px;
 `;
 
 export function About(props: AboutProps) {
   return (
     <StyledAbout>
-      <h1>Welcome to About!</h1>
       <Vectors />
+
+      <MainContent />
     </StyledAbout>
   );
 }
---
+++ src/components/sections/about/main-content.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+const StyledMainContent = styled.div``;
+
+const SubTitle = styled.h3`
+  color: ${({ theme }) => theme.palette.text.blue};
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 54px;
+  margin: 0 0 2px 0;
+`;
+
+const Title = styled.h2`
+  color: ${({ theme }) => theme.palette.text.primary};
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 54px;
+  max-width: 553px;
+  margin: 0 0 33px 0;
+`;
+
+const Text = styled.p`
+  max-width: 511px;
+  margin: 0 0 40px 0;
+`;
+
+export function MainContent() {
+  return (
+    <StyledMainContent>
+      <SubTitle>At Tokners are</SubTitle>
+
+      <Title>
+        Reimagining social media through the power of the blockchain.
+      </Title>
+
+      <Text>
+        We are creating social media 3.0 with influencers, celebrities and
+        creators being able to launch their own digital currency by simply
+        creating a profile with media content posted as Non fungible Tokens that
+        can be owned and traded on the Tokners network
+      </Text>
+    </StyledMainContent>
+  );
+}
+
+export default MainContent;

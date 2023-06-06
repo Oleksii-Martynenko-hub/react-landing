6d53ac89efd898fe7f45e9585372bf051b9e91df
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-06 21:51:29 +0300

    Modified Logo by adding props for white version, changed img format from PNG to SVG to reduce its size.

--- src/components/common/logo/logo.tsx
+++ src/components/common/logo/logo.tsx
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
-export interface LogoProps {}
+export interface LogoProps extends React.HTMLAttributes<HTMLImageElement> {
+  isLight?: boolean;
+}
 
 const StyledLogo = styled.img``;
 
-export function Logo(props: LogoProps) {
-  return (
+export function Logo({ isLight = false, ...props }: LogoProps) {
+  return !isLight ? (
     <StyledLogo
       src="src/assets/images/logo_full.png"
       alt="logo with dark text"
+      {...props}
+    />
+  ) : (
+    <StyledLogo
+      src="src/assets/images/footer/logo_white_text.png"
+      alt="logo with white text"
+      {...props}
     />
   );
 }

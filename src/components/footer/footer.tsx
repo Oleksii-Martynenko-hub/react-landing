c735dc44eef3f02830b8655afeeda3f86d280d9e
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-06 21:53:50 +0300

    Added Logo, list of social links and copyright to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import CommonContainer from '../common/common-container';
+
+import CommonContainer from 'src/components/common/common-container';
+import Logo from 'src/components/common/logo/logo';
 
 /* eslint-disable-next-line */
 export interface FooterProps {}
@@ -14,12 +16,14 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const Navigation = styled.ul`
   display: flex;
   justify-content: center;
   flex: 1 1 100%;
+  margin: 0 0 121px 0;
 `;
 
 const NavItem = styled.li`
@@ -48,6 +52,50 @@ const NavLink = styled.a`
   border-bottom: 1px solid ${({ theme }) => theme.palette.text.light};
 `;
 
+const SocialList = styled.ul`
+  display: flex;
+  padding: 0 0 0 18px;
+`;
+
+const SocialItem = styled.li`
+  margin: 0 10px 0 0;
+`;
+
+const SocialLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 2px solid rgba(255, 255, 255, 0.05);
+`;
+
+const SocialIcon = styled.img``;
+
+const Copyright = styled.p`
+  color: ${({ theme }) => theme.palette.text.light};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: right;
+  letter-spacing: 0.1px;
+`;
+
+const Designer = styled.span`
+  display: block;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 140%;
+  letter-spacing: 0.02em;
+  text-decoration-line: underline;
+  opacity: 0.5;
+`;
+
+const DesignerLogo = styled.img`
+  margin: 0 6px 0 0;
+`;
+
 export function Footer(props: FooterProps) {
   const navList = [
     {
@@ -68,6 +116,39 @@ export function Footer(props: FooterProps) {
     },
   ];
 
+  const socialList = [
+    {
+      name: 'Twitter',
+      icon: 'src/assets/images/footer/twitter.svg',
+      link: 'https://twitter.com/NFT_Art_Club',
+    },
+    {
+      name: 'Telegram',
+      icon: 'src/assets/images/footer/telegram.svg',
+      link: 'https://t.me/NFTArtClub',
+    },
+    {
+      name: 'Instagram',
+      icon: 'src/assets/images/footer/instagram.svg',
+      link: 'https://www.instagram.com/nftartclub/',
+    },
+    {
+      name: 'Medium',
+      icon: 'src/assets/images/footer/medium.svg',
+      link: 'https://medium.com/@nftartclub',
+    },
+    {
+      name: 'Github',
+      icon: 'src/assets/images/footer/github.svg',
+      link: 'https://github.com/NFTArtClub',
+    },
+    {
+      name: 'Reddit',
+      icon: 'src/assets/images/footer/reddit.svg',
+      link: 'https://www.reddit.com/r/NFTArtClub/',
+    },
+  ];
+
   return (
     <StyledFooter>
       <CommonContainer>
@@ -79,6 +160,29 @@ export function Footer(props: FooterProps) {
               </NavItem>
             ))}
           </Navigation>
+
+          <Logo isLight />
+
+          <SocialList>
+            {socialList.map((item) => (
+              <SocialItem key={item.name}>
+                <SocialLink href={item.link}>
+                  <SocialIcon src={item.icon} alt={item.name + ' icon'} />
+                </SocialLink>
+              </SocialItem>
+            ))}
+          </SocialList>
+
+          <Copyright>
+            © 2021 Tokners. All rights reserved.
+            <Designer>
+              <DesignerLogo
+                src="src/assets/images/footer/designer_logo.svg"
+                alt="designer logo"
+              />
+              Design by Oleg Kulik
+            </Designer>
+          </Copyright>
         </ContentWrapper>
       </CommonContainer>
     </StyledFooter>
